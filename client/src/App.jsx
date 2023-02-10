e7d4a676fd25c5ab36d7f96928945bfd5f591211
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-10 20:47:56 +0700

    update-App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,51 @@
-import React ,{ useState } from 'react';
+import React ,{ useState, Suspense, lazy } from 'react';
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import SignIn from "./pages/SignIn";
+
+const Container = styled.div`
+  display: flex;`
+;
+
+const Main = styled.div`
+  flex: 7;
+`;
+
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
 
 function App() {
 
   return (
-    <div className="App">
-      h1
-    </div>
+    <>
+      <Container>
+        <BrowserRouter>
+          <Menu />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                {/* =================RouterComponents====================== */}
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="signin" element={<SignIn />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                  </Route>
+                </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
+    </>
   )
 }
 
