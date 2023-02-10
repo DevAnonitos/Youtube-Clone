fe19c7ef8a4fc409e4284dfb29d401b3aeecf612
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-10 23:10:20 +0700

    update-Components

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <>
+        <div className="loader">
+            <svg viewBox="0 0 80 80">
+                <circle id="test" cx="40" cy="40" r="32"></circle>
+            </svg>
+        </div>
+    </>
+  )
+}
+
+export default Loader
