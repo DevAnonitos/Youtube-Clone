7baec17171e9db5bfd29ce799c09829f14333a36
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-11 22:53:40 +0700

    update-FixMenu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,11 @@ import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CircularProgress from '@mui/material/CircularProgress';
 
-import Home from "./pages/Home";
-import Video from "./pages/Video";
-import SignIn from "./pages/SignIn";
+const HomeView = lazy(() => delayView(import('./pages/Home')));
+const SignInView = lazy(() =>delayView(import('./pages/Video')));
+const VideoView = lazy(() => delayView(import('./pages/Video')));
 
 const Container = styled.div`
   display: flex;`
@@ -21,6 +22,12 @@ const Wrapper = styled.div`
   padding: 22px 96px;
 `;
 
+function delayView(promise) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 1500);
+  }).then(() => promise);
+}
+
 function App() {
 
   return (
@@ -31,16 +38,27 @@ function App() {
           <Main>
             <Navbar />
             <Wrapper>
-              <Routes>
-                {/* =================RouterComponents====================== */}
-                <Route path="/">
-                  <Route index element={<Home />} />
-                  <Route path="signin" element={<SignIn />} />
-                  <Route path="video">
-                    <Route path=":id" element={<Video />} />
-                  </Route>
+              <Suspense
+                fallback={
+                  <div
+                    className='flex justify-center
+                    items-center min-h-screen text-3xl'
+                  >
+                    <CircularProgress />
+                  </div>
+                }
+              >
+                <Routes>
+                  {/* =================RouterComponents====================== */}
+                  <Route path="/">
+                    <Route index element={<HomeView />} />
+                    <Route path="signin" element={<SignInView />} />
+                    <Route path="video">
+                      <Route path=":id" element={<VideoView />} />
+                    </Route>
                   </Route>
                 </Routes>
+              </Suspense>
             </Wrapper>
           </Main>
         </BrowserRouter>
