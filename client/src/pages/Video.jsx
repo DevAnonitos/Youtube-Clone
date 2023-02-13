93713b6a229a8780112b46a01953f717c62ff038
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-13 09:50:53 +0700

    update-Video@1

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+
+const VideoWrapper = styled.div``;
+
+const Title = styled.div``;
+const Info = styled.span``;
+const Buttons = styled.div``;
+const Button = styled.div``;
+
+const Recommentdation = styled.div`
+  flex: 2;
+`;
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <>
+      <Container>
+        <Content>
+          <VideoWrapper>
+            <iframe
+              width="100%"
+              height="720"
+              src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            >
+              
+            </iframe>
+          </VideoWrapper>
+        </Content>
+
+        <Recommentdation>
+          Recomments
+        </Recommentdation>
+      </Container>
+    </>
   )
 }
 
