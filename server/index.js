ad3ef9b8752f4a394018c89cd2df0830164b7774
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-16 08:54:47 +0700

    update-Model-MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+    mongoose.set('strictQuery', true);
+    mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        throw err;
+    })
+}
 
 app.listen(8000, () => {
-    console.log("Connected!");
+    connect();
+    console.log("Connected to server");
 });
---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+    },
+    subscribedUsers: {
+        type: [String],
+    },
+    fromGoogle: {
+        type: Boolean,
+        default: false,
+    },
+} ,{ timestamps: true });
+export default mongoose.model('User', UserSchema);
