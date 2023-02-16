f53a497c4967e530ded396b56250b70f47435de9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-16 21:54:41 +0700

    update-Controller

--- server/models/Video.js
+++ server/models/Video.js
@@ -3,40 +3,40 @@ import mongoose from "mongoose";
 const VideoSchema = new mongoose.Schema(
     {
         userId: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         title: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         desc: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         imgUrl: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         videoUrl: {
-        type: String,
-        required: true,
+            type: String,
+            required: true,
         },
         views: {
-        type: Number,
-        default: 0,
+            type: Number,
+            default: 0,
         },
         tags: {
-        type: [String],
-        default: [],
+            type: [String],
+            default: [],
         },
         likes: {
-        type: [String],
-        default: [],
+            type: [String],
+            default: [],
         },
         dislikes: {
-        type: [String],
-        default: [],
+            type: [String],
+            default: [],
         },
     },
     { timestamps: true },
