797f63fbd3b68b23d8c928d1587e2feeceaf3cb4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-22 22:43:13 +0700

    update-VideoAPI

---
+++ server/controllers/video.js
@@ -0,0 +1,53 @@
+import User from "../models/User.js";
+import Video from "../models/Video.js";
+import { createError } from "../error.js";
+// addVideo
+export const addVideo = async (req, res, next) => {
+    const newVideo = new Video({userId: req.user.id, ...req.body});
+    try {
+
+        const savedVideo = await newVideo.save();
+        res.status(200).json(savedVideo);
+
+    } catch (error) {
+        next(error);
+    }
+};
+// UpdateVideo
+export const updateVideo = async (req, res, next) => {
+    try {
+        const video = await Video.findById(req.params.id);
+
+        if(!video) return next(createError(404, "Video not found"));
+
+        if(req.user.id === video.userId) {
+            const updateVideo = await Video.findByIdAndUpdate(req.params.id,
+                {
+                    $set: req.body,
+                },
+                {new: true}
+            );
+            res.status(200).json(updateVideo);
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+// DeleteVideo
+export const deleteVideo = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+// GetVideo
+export const getVideo = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,17 @@
 import express from "express";
-import {  } from "../controllers/video.js";
+import {
+    addVideo,
+    updateVideo,
+    deleteVideo,
+    getVideo
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, updateVideo);
+router.delete("/:id", verifyToken, deleteVideo);
+router.get("/find/:id", verifyToken, getVideo);
 
 export default router;
