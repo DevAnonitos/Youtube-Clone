a4b229f73f558eab40c126357a0a40adc0c5964b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-24 20:54:02 +0700

    update-Comments

---
+++ server/controllers/comment.js
@@ -0,0 +1,28 @@
+import Comment from "../models/Comment.js";
+import Video from "../models/Video.js";
+import { createError } from "../error.js";
+
+// addComment
+export const addComment = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+};
+// DeleteComment
+export const deleteComment = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+};
+// getComments
+export const getComments = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+};
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,6 +1,11 @@
 import express from "express";
-import {  } from "../controllers/comment.js";
+import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addComment);
+router.delete("/:id", verifyToken, deleteComment);
+router.get("/:videoId", getComments);
 
 export default router;
