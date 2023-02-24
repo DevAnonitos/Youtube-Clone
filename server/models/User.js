ef861ed581a7d3574b78f3ca698ddd4a2e0fd255
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-24 17:56:25 +0700

    update-Models

--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,10 @@ const UserSchema = new mongoose.Schema({
     img: {
         type: String,
     },
+    subscribers: {
+        type: Number,
+        default: 0,
+    },
     subscribedUsers: {
         type: [String],
     },
