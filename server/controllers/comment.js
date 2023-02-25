a53fd32f3bdef96c9b278d680da3dd34ac148d3c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-25 23:20:21 +0700

    update-Comment@2

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -4,8 +4,10 @@ import { createError } from "../error.js";
 
 // addComment
 export const addComment = async (req, res, next) => {
+    const newComment = new Comment({...req.body, userId:req.user.id});
     try {
-
+        const saveComment = await newComment.save();
+        res.status(200).send(saveComment);
     } catch (error) {
         next(error);
     }
@@ -13,7 +15,14 @@ export const addComment = async (req, res, next) => {
 // DeleteComment
 export const deleteComment = async (req, res, next) => {
     try {
-
+        const comment = await Comment.findById(res.params.id);
+        const video =  await Video.findById(res.params.id);
+        if(req.user.id === comment.userId || req.user.id === video.userId){
+            await Comment.findByIdAndDelete(req.params.id);
+            res.status(200).json("The comment has been deleted!");
+        } else {
+            next(createError(403, "You can delete on your comments"));
+        }
     } catch (error) {
         next(error);
     }
@@ -21,7 +30,8 @@ export const deleteComment = async (req, res, next) => {
 // getComments
 export const getComments = async (req, res, next) => {
     try {
-
+        const comments = await Comment.find({ videoId: req.params.videoId });
+        res.status(200).json(comments);
     } catch (error) {
         next(error);
     }
