ed44f924592ee520222cb2d5e7a9ea9df308ab29
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-01 22:07:11 +0700

    update-UploadAndReComment

---
+++ client/src/components/Recommendation.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import styled from "styled-components";
+import Card from "./Card";
+
+
+const Recommendation = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Recommendation
